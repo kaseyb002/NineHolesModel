65a065bbfc14710c3c96a10ea5aa880756669337
Kasey Baughan <kaseyb002@yahoo.com>
2026-08-31 06:49:56 -0700

    swift 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -27,5 +27,4 @@ let package = Package(
             dependencies: ["NineHolesModel"]
         ),
     ],
-    swiftLanguageModes: [.v6]
 )
